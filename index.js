039c2f71596504d6de44a06f64fa741e1bc1d3d3
mehedyPUST <mehedy.pust@gmail.com>
2026-03-06 00:36:31 +0600

    modal

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const categoriesContainer = document.getElementById('categoriesContainer');
 const treesContainer = document.getElementById('treesContainer');
 const loadingSpinner = document.getElementById('loadingSpinner');
 const allTreesBtn = document.getElementById('allTreesBtn');
+const treeDetailsModal = document.getElementById('tree-details-modal');
+const modalImage = document.getElementById('modalImage');
+const modalCategory = document.getElementById('modalCategory');
+const modalDescription = document.getElementById('modalDescription');
+const modalPrice = document.getElementById('modalPrice');
+const modalTitle = document.getElementById('modalTitle');
 
 function showLoading() {
     loadingSpinner.classList.remove('hidden');
@@ -93,15 +99,15 @@ function displayTrees(trees) {
         const card = document.createElement('div');
         card.className = "card bg-white shadow-sm";
         card.innerHTML = `
-            <figure>
+            <figure onclick ="openTreeModal(${tree.id})">
                 <img src="${tree.image}"
                     alt="${tree.name}"
                     title="${tree.name}" 
-                    class="h-48 w-full object-cover"/>
+                    class="h-48 w-full object-cover cursor-pointer"/>
             </figure>
-            <div class="card-body">
-                <h2 class="card-title">${tree.name}</h2>
-                <p class=" line-clamp-3">${tree.description}</p>
+            <div class="card-body" >
+                <h2 class="card-title cursor-pointer" onclick ="openTreeModal(${tree.id})" >${tree.name}</h2>
+                <p class=" line-clamp-3 cursor-pointer" onclick ="openTreeModal(${tree.id})">${tree.description}</p>
                 <div class="badge badge-outline badge-success">${tree.category}</div>
                 <div class=" flex justify-between items-center gap-2">
                     <h2 class="font-bold text-xl text-green-500 ">$${tree.price}</h2>
@@ -113,7 +119,21 @@ function displayTrees(trees) {
 
     });
 }
+async function openTreeModal(treeId) {
+    console.log(treeId, "treeId");
+    const res = await fetch(`https://openapi.programming-hero.com/api/plant/${treeId}`);
+    const data = await res.json();
+    const plantDetails = data.plants;
+    console.log(plantDetails, 'data')
+    console.log(data, 'data');
 
+    modalTitle.textContent = plantDetails.name;
+    modalImage.src = plantDetails.image;
+    modalCategory.textContent = plantDetails.category;
+    modalPrice.textContent = plantDetails.price;
+    modalDescription.textContent = plantDetails.description;
+    treeDetailsModal.showModal();
+}
 
 loadCategories();
 loadTrees();
